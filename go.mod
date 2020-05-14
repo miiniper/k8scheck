2e5abc1c366907fd45674189e5ebd5c297ec68c7
hanlei3 <hanlei3@ifeng.com>
2020-05-14 11:11:15 +0800

    init service

---
+++ go.mod
@@ -0,0 +1,11 @@
+module k8scheck
+
+go 1.14
+
+require (
+	github.com/fsnotify/fsnotify v1.4.7
+	github.com/julienschmidt/httprouter v1.2.0
+	github.com/miiniper/loges v0.0.0-20200509053933-a4ef73281bc3
+	github.com/spf13/viper v1.7.0
+	go.uber.org/zap v1.15.0
+)
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"k8scheck/httpd"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/miiniper/loges"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+func main() {
+	fmt.Println("starting   server ...")
+	viper.SetConfigName("conf")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		loges.Loges.Info("Config file changed: ", zap.Any("", e.Name))
+	})
+
+	service, err := httpd.New(viper.GetString("server.ip"))
+	if err != nil {
+		panic(err)
+	}
+	err = service.Start()
+	if err != nil {
+		panic(err)
+	}
+	defer service.Close()
+
+	terminate := make(chan os.Signal, 1)
+	signal.Notify(terminate, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGKILL)
+	<-terminate
+
+}
